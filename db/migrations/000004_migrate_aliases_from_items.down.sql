006daa7fc4eb3a53f5882e1dd77ebbdad356466e
Leonid Kozarin <kozalo@sadbot.ru>
2023-03-15 05:40:36 +0300

    Wrap DML instructions of the migration into transactions

--- db/migrations/000004_migrate_aliases_from_items.down.sql
+++ db/migrations/000004_migrate_aliases_from_items.down.sql
@@ -1,12 +1,14 @@
 ALTER TABLE Items DROP CONSTRAINT IF EXISTS fk_item_alias;
 ALTER TABLE Items ALTER COLUMN alias TYPE varchar(128) USING alias::varchar(128);
 
-UPDATE Items
-SET alias = (
-    SELECT a.name
-    FROM Aliases AS a
-    WHERE a.id = alias::bigint
-);
+BEGIN TRANSACTION ;
+    UPDATE Items
+    SET alias = (
+        SELECT a.name
+        FROM Aliases AS a
+        WHERE a.id = alias::bigint
+    );
 
--- noinspection SqlWithoutWhere
-DELETE FROM Aliases;
+    -- noinspection SqlWithoutWhere
+    DELETE FROM Aliases;
+COMMIT ;
--- db/migrations/000004_migrate_aliases_from_items.up.sql
+++ db/migrations/000004_migrate_aliases_from_items.up.sql
@@ -1,12 +1,14 @@
-INSERT INTO Aliases (name)
-SELECT DISTINCT alias FROM Items;
+BEGIN TRANSACTION ;
+    INSERT INTO Aliases (name)
+    SELECT DISTINCT alias FROM Items;
 
-UPDATE Items
-SET alias = (
-    SELECT a.id
-    FROM Aliases AS a
-    WHERE a.name = alias
-);
+    UPDATE Items
+    SET alias = (
+        SELECT a.id
+        FROM Aliases AS a
+        WHERE a.name = alias
+    );
+COMMIT ;
 
 ALTER TABLE Items ALTER COLUMN alias TYPE bigint USING alias::bigint;
 ALTER TABLE Items ADD CONSTRAINT fk_item_alias FOREIGN KEY (alias) REFERENCES Aliases(id);
